4517de748ce91d696bdc5bb3ea2191c5d45e7cdf
Chi <chiyantsestudio@gmail.com>
2023-01-06 14:38:40 -0500

    add gradient animation bg and input for bill and tip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Bill</label>
+      <input type="number" placeholder="0.00" />
+      <label>Tip</label>
+      <input type="number" placeholder='0' />
+      
     </div>
   );
 }
