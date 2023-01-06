3c5d18f93d057031dbb9c833c8423f328916feaa
Chi <chiyantsestudio@gmail.com>
2023-01-06 18:22:54 -0500

    add and style plus and Minus button for num of people, result space for the split total. Import useState to make the Plus and Minus buttons work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const[bill, setBill] = useState('');
+  const[tip, setTip] = useState('');
+  const[numOfPpl, setNumOfPpl] = useState(1);
+
+  function numOfPplPlus(){
+    setNumOfPpl(oldValue => oldValue +1);
+  }
+
+  function numOfPplMinus(){
+    setNumOfPpl(oldValue => {
+      if (oldValue === 1) {
+        return 1;
+      } else {
+        return oldValue -1 
+    }
+    })
+  }
+
   return (
     <div>
       <label>Bill</label>
-      <input type="number" placeholder="0.00" />
+      <span>$</span>
+      <input type="number" placeholder="0.00" min="0"
+      value={bill} onChange={ e => setBill(e.target.value)}
+      />
       <label>Tip</label>
-      <input type="number" placeholder='0' />
-      
+      <input type="number" placeholder='0' min="0"
+      value={tip} onChange={ e => setTip(e.target.value)}/>
+      <span>%</span>
+      <div className='summary'>
+
+        <div className='num-of-people'>
+          <label>No. of People</label>
+          <div className='num-of-people-control'>
+           <button onClick={numOfPplMinus}>-</button>
+           <span>{numOfPpl}</span>
+           <button onClick={numOfPplPlus}>+</button>
+          </div>
+        </div>
+          <div className='result'>
+            <label>Split total</label>
+            <span>0</span>
+        </div>
+
+      </div>
+
     </div>
   );
 }
